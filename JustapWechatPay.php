f334b476734dc11a0068dfed117d0bd38c9e6003
ethanjustap <ethan@justap.net>
2022-05-31 15:46:48 +0800

    更新公众号支付版本号

--- JustapWechatPay.php
+++ JustapWechatPay.php
@@ -767,7 +767,7 @@ if (!class_exists('JustapBaseJustapWechatPay')) {
             // 基础信息
             $base = [
                 'name'          => '开源聚合支付',  // 插件名称
-                'version'       => 'v1.5.1',  // 插件版本
+                'version'       => 'v1.5.2',  // 插件版本
                 'apply_version' => '不限',  // 适用系统版本描述
                 'apply_terminal' => ['pc', 'h5', 'ios', 'android', 'alipay', 'weixin'], // 适用终端 默认全部 ['pc', 'h5', 'ios', 'android', 'alipay', 'weixin', 'baidu', 'toutiao']
                 'desc'          => '<h1>开源聚合(TM)支付</h1>
